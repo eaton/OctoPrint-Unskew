5c7decc0ca38dc41966707086b6c118eeab8df57
Jeff Eaton <eaton@lullabot.com>
2018-03-13 10:52:26 -0500

    Converting from realtime to on-upload conversion

--- octoprint_unskew/__init__.py
+++ octoprint_unskew/__init__.py
@@ -3,8 +3,11 @@ from __future__ import absolute_import
 
 import re
 import octoprint.plugin
+import octoprint.filemanager
+import octoprint.filemanager.util
 
-class UnskewPlugin(octoprint.plugin.TemplatePlugin,
+class UnskewPlugin(octoprint.filemanager.util.AbstractFileWrapper,
+                   octoprint.plugin.TemplatePlugin,
                    octoprint.plugin.SettingsPlugin):
 
 	def get_settings_defaults(self):
@@ -20,7 +23,13 @@ class UnskewPlugin(octoprint.plugin.TemplatePlugin,
 			dict(type="settings", custom_bindings=False)
 		]
 
-	def compensate_for_skew(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+	def get_version(self):
+		return self._plugin_version
+
+	def unskew_coordinates(path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
+
+		if not octoprint.filemanager.valid_file_type(path, type="gcode"):
+			return file_object
 
 		xyerr = self._settings.get(["xyerr"])
 		yzerr = self._settings.get(["yzerr"])
@@ -51,51 +60,53 @@ class UnskewPlugin(octoprint.plugin.TemplatePlugin,
 		if not zxtan == 0:
 			self._logger.info("Unskew: The ZX error is set to %s degrees" % zxtan)
 
-
 		xin = 0.0
 		yin = 0.0
 		zin = 0.0
+		
+		outfile = octoprint.filemanager.util.AbstractFileWrapper(file_object.filename)
 
-		if gcode:
-			gmatch = re.match(r'G[0-1]',gcode,re.I)
+		for line in file_object:
+			# Check that the current 'line' is a move, if so the line is processed
+			gmatch = re.match(r'G[0-1]',line,re.I)
 			if gmatch:
 
-					# load the incoming X coordinate into a variable. Previous value will be used if new value is not found.
-					xsrch = re.search(r'[xX]\d*\.*\d*',line,re.I)
-					if xsrch: # if an X value is found
-							xin = float(re.sub(r'[xX]','',xsrch.group())) # Strip the letter from the coordinate.
+				# load the incoming X coordinate into a variable. Previous value will be used if new value is not found.
+				xsrch = re.search(r'[xX]\d*\.*\d*',line,re.I)
+				if xsrch: # if an X value is found
+						xin = float(re.sub(r'[xX]','',xsrch.group())) # Strip the letter from the coordinate.
 
-					# load the incoming Y coordinate into a variable. Previous value will be used if new value is not found.
-					ysrch = re.search(r'[yY]\d*\.*\d*', line, re.I)
-					if ysrch:
-							yin = float(re.sub(r'[yY]','',ysrch.group())) # Strip the letter from the coordinate.
+				# load the incoming Y coordinate into a variable. Previous value will be used if new value is not found.
+				ysrch = re.search(r'[yY]\d*\.*\d*', line, re.I)
+				if ysrch:
+						yin = float(re.sub(r'[yY]','',ysrch.group())) # Strip the letter from the coordinate.
 
-					# load the incoming Z coordinate into a variable. Previous value will be used if new value is not found.
-					zsrch = re.search(r'[zZ]\d*\.*\d*', line, re.I)
-					if zsrch:
-							zin = float(re.sub(r'[zZ]','',zsrch.group())) # Strip the letter from the coordinate.
+				# load the incoming Z coordinate into a variable. Previous value will be used if new value is not found.
+				zsrch = re.search(r'[zZ]\d*\.*\d*', line, re.I)
+				if zsrch:
+						zin = float(re.sub(r'[zZ]','',zsrch.group())) # Strip the letter from the coordinate.
 
-					# calculate the corrected/skewed XYZ coordinates
-					xout = round(xin-yin*xytan,3)
-					yout = round(yin-zin*yztan,3)
-					xout = round(xout-zin*zxtan,3)
-					zout = zin # Z coodinates must remain the same to prevent layers being tilted!
+				# calculate the corrected/skewed XYZ coordinates
+				xout = round(xin-yin*xytan,3)
+				yout = round(yin-zin*yztan,3)
+				xout = round(xout-zin*zxtan,3)
+				zout = zin # Z coodinates must remain the same to prevent layers being tilted!
 
-					lineout = cmd
+				lineout = line
+				if xsrch:
+						lineout = re.sub(r'[xX]\d*\.*\d*', 'X' + str(xout), cmd)
 
-					if xsrch:
-							cmd = re.sub(r'[xX]\d*\.*\d*', 'X' + str(xout), cmd)
+				if ysrch:
+						lineout = re.sub(r'[yY]\d*\.*\d*', 'Y' + str(yout), cmd)
 
-					if ysrch:
-							cmd = re.sub(r'[yY]\d*\.*\d*', 'Y' + str(yout), cmd)
+				if zsrch:
+						lineout = re.sub(r'[zZ]\d*\.*\d*', 'Z' + str(zout), cmd)
 
-					if zsrch:
-							cmd = re.sub(r'[zZ]\d*\.*\d*', 'Z' + str(zout), cmd)
+				outfile.write(lineout)
+			else:
+				outfile.write(line)
 
-		return cmd,
-
-	def get_version(self):
-		return self._plugin_version
+		return outfile
 
 __plugin_name__ = "Unskew"
 
@@ -105,5 +116,5 @@ def __plugin_load__():
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
-		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.compensate_for_skew,
+    "octoprint.filemanager.preprocessor": __plugin_implementation__.unskew_coordinates
 	}
